6faec5f6dfaa6f0d3c49e4d06450bc970cacca39
kimbow <178950763@qq.com>
2019-06-12 20:16:27 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,19 @@
+package com.itheima.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("乱花渐欲迷人眼");
+        System.out.println("浅草才能没马蹄");
+        System.out.println("曾经沧海难为水");
+        System.out.println("除却巫山不是云");
+        System.out.println("钟山风雨起苍黄");
+        System.out.println("百万雄师过大江");
+        System.out.println("大鹏一日同风起");
+        System.out.println("扶摇直上九万里");
+        System.out.println("玲珑骰子安红豆");
+        System.out.println("入骨相思知不知");
+        System.out.println("从此无心爱凉月");
+        System.out.println("任他明月下西楼");
+    }
+}
+//创建git Repository时,只有选择上层目录,创建出来的git文件夹才能处于工作目录,这样可以省去很多麻烦
